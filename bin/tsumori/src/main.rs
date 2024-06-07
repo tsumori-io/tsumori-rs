f42fb1a7707d9630c9525afef1d596d320551a1f
zees-dev <dev.786zshan@gmail.com>
2024-06-07 16:08:55 +1200

    bridge crate -> tsumori

--- bin/tsumori/src/main.rs
+++ bin/tsumori/src/main.rs
@@ -6,7 +6,7 @@ use clap::{Parser, Subcommand, ValueEnum};
 pub(crate) const SHORT_VERSION: &str = concat!("v", env!("CARGO_PKG_VERSION"));
 
 #[derive(Parser, Debug)]
-#[command(author, version = SHORT_VERSION, long_version = SHORT_VERSION, about = "tsumori-bridge", long_about = None)]
+#[command(author, version = SHORT_VERSION, long_version = SHORT_VERSION, about = "tsumori-rs", long_about = None)]
 struct Cli {
     /// The command to run
     #[clap(subcommand)]

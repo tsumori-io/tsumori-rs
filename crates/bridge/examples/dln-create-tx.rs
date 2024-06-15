7f5fa070d9cef452080a5d97b57cc52a7965d7d5
zees-dev <dev.786zshan@gmail.com>
2024-06-15 17:11:27 +1200

    updated dln create tx example

--- crates/bridge/examples/dln-create-tx.rs
+++ crates/bridge/examples/dln-create-tx.rs
@@ -1,9 +1,24 @@
+use bridge::across::QuoteQueryParams;
 use reqwest::blocking::Client;
 use serde::Deserialize;
-use std::collections::HashMap;
+use std::{borrow::Cow, collections::HashMap};
 
 /// https://docs.dln.trade/dln-api/quick-start-guide/requesting-order-creation-transaction
 /// https://api.dln.trade/v1.0/#/DLN/DlnOrderControllerV10_createOrder
+
+#[derive(Debug, serde::Serialize, Clone)]
+#[serde(rename_all = "camelCase")]
+pub struct CreateTxQueryParams<'a> {
+    src_chain_id: u32,
+    src_chain_token_in: &'a str,
+    src_chain_token_in_amount: Cow<'a, str>,
+    dst_chain_id: u32,
+    dst_chain_token_out: &'a str,
+    dst_chain_token_out_recipient: &'a str,
+    src_chain_order_authority_address: &'a str,
+    dst_chain_order_authority_address: &'a str,
+    external_call: Option<&'a String>,
+}
 
 #[derive(Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
@@ -42,7 +57,7 @@ struct Estimation {
     recommended_slippage: f64,
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(serde::Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 struct TxData {
     data: String,
@@ -71,51 +86,22 @@ struct CreateTxResponse {
 }
 
 fn main() {
-    let src_chain_id = 42161; // Arbitrum
-    let src_chain_token_in = "0xaf88d065e77c8cC2239327C5EDb3A432268e5831"; // USDC
-    let src_chain_token_in_amount = 4_000_000u32; // 4 USDC
-    let dst_chain_id = 8453; // Base
-    let dst_chain_token_out = "0xd9aAEc86B65D86f6A7B5B1b0c42FFA531710b6CA"; // USDCbC Base
-
-    // let dst_chain_token_out_amount = 99623050_u32; // optional
-
-    let dst_chain_token_out_recipient = "0xD79842424f797feF2B713BAd555eDdD0b6c89a80";
-
-    // same EOA account has authority on both chains
-    let src_chain_order_authority_address = "0xD79842424f797feF2B713BAd555eDdD0b6c89a80";
-    let dst_chain_order_authority_address = "0xD79842424f797feF2B713BAd555eDdD0b6c89a80";
-
-    // Build the request URL
-    let mut params = HashMap::new();
-    params.insert("srcChainId", src_chain_id.to_string());
-    params.insert("srcChainTokenIn", src_chain_token_in.to_string());
-    params.insert(
-        "srcChainTokenInAmount",
-        src_chain_token_in_amount.to_string(),
-    );
-    params.insert("dstChainId", dst_chain_id.to_string());
-    params.insert("dstChainTokenOut", dst_chain_token_out.to_string());
-    // params.insert(
-    //     "dstChainTokenOutAmount",
-    //     dst_chain_token_out_amount.to_string(),
-    // );
-    params.insert(
-        "dstChainTokenOutRecipient",
-        dst_chain_token_out_recipient.to_string(),
-    );
-    params.insert(
-        "srcChainOrderAuthorityAddress",
-        src_chain_order_authority_address.to_string(),
-    );
-    params.insert(
-        "dstChainOrderAuthorityAddress",
-        dst_chain_order_authority_address.to_string(),
-    );
+    let query_params = CreateTxQueryParams {
+        src_chain_id: 42161,                                               // Arbitrum
+        src_chain_token_in: "0xaf88d065e77c8cC2239327C5EDb3A432268e5831",  // USDC
+        src_chain_token_in_amount: "4000000".into(),                       // 4 USDC
+        dst_chain_id: 8453,                                                // Base
+        dst_chain_token_out: "0xd9aAEc86B65D86f6A7B5B1b0c42FFA531710b6CA", // USDCbC Base
+        dst_chain_token_out_recipient: "0xD79842424f797feF2B713BAd555eDdD0b6c89a80", // recipient
+        src_chain_order_authority_address: "0xD79842424f797feF2B713BAd555eDdD0b6c89a80", // same EOA account has authority on both chains
+        dst_chain_order_authority_address: "0xD79842424f797feF2B713BAd555eDdD0b6c89a80", // same EOA account has authority on both chains
+        external_call: None,
+    };
 
     let client = Client::new();
     let response = client
         .get("https://api.dln.trade/v1.0/dln/order/create-tx")
-        .query(&params)
+        .query(&query_params)
         .send()
         .unwrap();
 
